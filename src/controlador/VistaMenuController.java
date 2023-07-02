0999f1fd71184ffd277fad09b819a9e94d1a1650
unknown <mateofajardo25@gmail.com>
2023-07-02 12:40:36 -0500

    Se guardan cambios 020723

--- src/controlador/VistaMenuController.java
+++ src/controlador/VistaMenuController.java
@@ -6,12 +6,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
@@ -25,6 +22,7 @@ public class VistaMenuController implements Initializable {
 
     @FXML
     private Button btnNewGame;
+    
     @FXML
     private Button btnExit;
    
